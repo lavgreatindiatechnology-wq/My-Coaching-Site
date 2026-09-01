03e21fe16f386b2f9b32eed781313d0bbae8112b
lavgreatindiatechnology-wq <lavgreatindiatechnology@gmail.com>
2026-09-01 13:59:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL="https://erizrscbaugstzpurvs.supabase.co";
+const SUPABASE_URL="https://erizrscbaugusfzpurvs.supabase.co";
 const SUPABASE_KEY="sb_publishable_tbrLP72CX3nnL-MyrVqZ_Q_Q644d9il";
